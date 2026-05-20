6ce557c6140c5813d744c3249329b5a7c2dfed33
Vlad <vladkylakoff@gmail.com>
2026-05-20 19:08:09 +0300

    fix code

--- models/page_object_models.py
+++ models/page_object_models.py
@@ -1,3 +1,5 @@
+from tabnanny import check
+from pytest_check import check
 import allure
 from playwright.sync_api import Page, expect
 
@@ -8,28 +10,33 @@ class PageAction:
 
     @allure.step("Переход на страницу: {url}")
     def open_url(self, url: str):
-        self.page.goto(url)
+        with check:
+            self.page.goto(url)
 
     @allure.step("Ввод текста '{text}' в поле '{locator}'")
     def enter_text_to_element(self, locator: str, text: str):
-        self.page.fill(locator, text)
+        with check:
+            self.page.fill(locator, text)
 
     @allure.step("Клик по элементу '{locator}'")
     def click_element(self, locator: str):
-        self.page.click(locator)
+        with check:
+            self.page.click(locator)
 
     @allure.step("Ожидание загрузки страницы: {url}")
     def wait_redirect_for_url(self, url: str):
-        self.page.wait_for_url(url)
-        assert self.page.url == url, "Редирект на домашнюю старницу не произошел"
+        with check:
+            self.page.wait_for_url(url)
 
     @allure.step("Получение текста элемента: {locator}")
     def get_element_text(self, locator: str) -> str:
-        return self.page.locator(locator).text_content()
+        with check:
+            return self.page.locator(locator).text_content()
 
     @allure.step("Ожидание появления или исчезновения элемента: {locator}, state = {state}")
     def wait_for_element(self, locator: str, state: str = "visible"):
-        self.page.locator(locator).wait_for(state=state)
+        with check:
+            self.page.locator(locator).wait_for(state=state)
 
     @allure.step("Скриншот текущей страиницы")
     def make_screenshot_and_attach_to_allure(self):
@@ -43,22 +50,21 @@ class PageAction:
     def check_pop_up_element_with_text(self, text: str):
         with allure.step("Проверка появления алерта с текстом: '{text}'"):
             notification_locator = self.page.get_by_text(text)
-            # Ждем появления элемента
-            notification_locator.wait_for(state="visible")
-            assert notification_locator.is_visible(), "Уведомление не появилось"
-
+            with check:
+                notification_locator.wait_for(state="visible")
         with allure.step("Проверка исчезновения алерта с текстом: '{text}'"):
-            # Ждем, пока предупреждение исчезнет
-            notification_locator.wait_for(state="hidden")
-            assert notification_locator.is_visible() == False, "Уведомление не исчезло"
+            with check:
+                notification_locator.wait_for(state="hidden")
 
     @allure.step("Проверка видимости элемента {locator} на странице")
     def visibility_check(self, locator: str):
-        expect(self.page.locator(locator).first).to_be_visible()
+        with check:
+            expect(self.page.locator(locator).first).to_be_visible()
 
     @allure.step("Проверка disabled элемента {locator} на странице")
     def not_visible_check(self, locator: str):
-        expect(self.page.locator(locator).first).to_be_hidden()
+        with check:
+            expect(self.page.locator(locator).first).to_be_hidden()
 
 
 class BasePage(PageAction): #Базовая логика доспустимая для всех страниц на сайте
--- models/ui_page_object/сinescop_review_page.py
+++ models/ui_page_object/сinescop_review_page.py
@@ -29,115 +29,67 @@ class CinescopReviewPage(BasePage):
 
     @allure.step("Открытие страницы Фильм с Авторизацией")
     def open_via_auth(self, email, password):
-        with check:
-            self.open_url(self.home_url)
-        with check:
-            self.click_element(self.home_login_button)
-        with check:
-            self.enter_text_to_element(self.login_email_input, email)
-        with check:
-            self.enter_text_to_element(self.login_password_input, password)
-        with check:
-            self.click_element(self.login_button)
-        with check:
-            self.click_element(self.movie_button)
+        self.open_url(self.home_url)
+        self.click_element(self.home_login_button)
+        self.enter_text_to_element(self.login_email_input, email)
+        self.enter_text_to_element(self.login_password_input, password)
+        self.click_element(self.login_button)
+        self.click_element(self.movie_button)
 
     @allure.step("Открытие страницы Фильм без Авторизации")
     def open(self):
-        with check:
-            self.open_url(self.home_url)
-        with check:
-            self.click_element(self.movie_button)
+        self.open_url(self.home_url)
+        self.click_element(self.movie_button)
 
 
     @allure.step("Проверка видимости всех элементов страницы фильма без Авторизацией")
     def all_elements_visibility_check_not_auth(self):
-        with check:
-            self.visibility_check(self.home_button)
-        with check:
-            self.visibility_check(self.all_movies_button)
-        with check:
-            self.not_visible_check(self.user_icon)
-        with check:
-            self.visibility_check(self.title)
-        with check:
-            self.visibility_check(self.description)
-        with check:
-            self.visibility_check(self.genre)
-        with check:
-            expect(self.page.locator(self.rating).first).to_be_visible()
-        with check:
-            expect(self.page.locator(self.rating_value).first).to_be_visible()
-        with check:
-            self.visibility_check(self.poster)
-        with check:
-            self.visibility_check(self.buy_button)
-        with check:
-            self.visibility_check(self.cart_icon)
-        with check:
-            self.visibility_check(self.price)
-        with check:
-            self.visibility_check(self.reviews_section)
-        with check:
-            self.visibility_check(self.review_author)
-        with check:
-            self.visibility_check(self.review_text)
-        with check:
-            self.visibility_check(self.review_rating)
-        with check:
-            self.not_visible_check(self.review_textarea_placeholder)
-        with check:
-            self.not_visible_check(self.review_submit_button)
+        self.visibility_check(self.home_button)
+        self.visibility_check(self.all_movies_button)
+        self.not_visible_check(self.user_icon)
+        self.visibility_check(self.title)
+        self.visibility_check(self.description)
+        self.visibility_check(self.genre)
+        expect(self.page.locator(self.rating).first).to_be_visible()
+        expect(self.page.locator(self.rating_value).first).to_be_visible()
+        self.visibility_check(self.poster)
+        self.visibility_check(self.buy_button)
+        self.visibility_check(self.cart_icon)
+        self.visibility_check(self.price)
+        self.visibility_check(self.reviews_section)
+        self.visibility_check(self.review_author)
+        self.visibility_check(self.review_text)
+        self.visibility_check(self.review_rating)
+        self.not_visible_check(self.review_textarea_placeholder)
+        self.not_visible_check(self.review_submit_button)
 
     @allure.step("Проверка видимости всех элементов страницы фильма с Авторизацией")
     def all_elements_visibility_check_auth(self):
-        with check:
-            self.visibility_check(self.home_button)
-        with check:
-            self.visibility_check(self.all_movies_button)
-        with check:
-            self.visibility_check(self.user_icon)
-        with check:
-            self.visibility_check(self.profile_button)
-        with check:
-            self.visibility_check(self.title)
-        with check:
-            self.visibility_check(self.description)
-        with check:
-            self.visibility_check(self.genre)
-        with check:
-            expect(self.page.locator(self.rating).first).to_be_visible()
-        with check:
-            expect(self.page.locator(self.rating_value).first).to_be_visible()
-        with check:
-            self.visibility_check(self.poster)
-        with check:
-            self.visibility_check(self.buy_button)
-        with check:
-            self.visibility_check(self.cart_icon)
-        with check:
-            self.visibility_check(self.price)
-        with check:
-            self.visibility_check(self.reviews_section)
-        with check:
-            self.visibility_check(self.review_author)
-        with check:
-            self.visibility_check(self.review_text)
-        with check:
-            self.visibility_check(self.review_rating)
-        with check:
-            self.visibility_check(self.review_textarea_placeholder)
-        with check:
-            self.visibility_check(self.review_submit_button)
+        self.visibility_check(self.home_button)
+        self.visibility_check(self.all_movies_button)
+        self.visibility_check(self.user_icon)
+        self.visibility_check(self.profile_button)
+        self.visibility_check(self.title)
+        self.visibility_check(self.description)
+        self.visibility_check(self.genre)
+        expect(self.page.locator(self.rating).first).to_be_visible()
+        expect(self.page.locator(self.rating_value).first).to_be_visible()
+        self.visibility_check(self.poster)
+        self.visibility_check(self.buy_button)
+        self.visibility_check(self.cart_icon)
+        self.visibility_check(self.price)
+        self.visibility_check(self.reviews_section)
+        self.visibility_check(self.review_author)
+        self.visibility_check(self.review_text)
+        self.visibility_check(self.review_rating)
+        self.visibility_check(self.review_textarea_placeholder)
+        self.visibility_check(self.review_submit_button)
 
     @allure.step("Добавляем и отправляем отзыв")
     def fill_review(self, text):
-        with check:
-            self.enter_text_to_element(self.review_textarea_placeholder, text)
-        with check:
-            self.click_element(self.review_submit_button)
+        self.enter_text_to_element(self.review_textarea_placeholder, text)
+        self.click_element(self.review_submit_button)
 
     @allure.step("Проверяем всплывающие окно")
     def check_pop_up_message(self):
-        with check:
-            self.check_pop_up_element_with_text("Отзыв успешно создан")
+        self.check_pop_up_element_with_text("Отзыв успешно создан")
