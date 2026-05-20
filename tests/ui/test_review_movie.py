157378bda54471d605fd516e07f0a6254866436d
Vlad <vladkylakoff@gmail.com>
2026-05-20 08:33:55 +0300

    fix code

--- tests/ui/test_review_movie.py
+++ tests/ui/test_review_movie.py
@@ -10,21 +10,15 @@ class TestFeedbackPage:
     @allure.title("Оставляем отзыв под фильмом")
     def test_feedback_page(self, page: Page, registered_user):
         page = CinescopReviewPage(page)
-        with check:
-            page.open_via_auth(registered_user.email, registered_user.password)
-        with check:
-            page.all_elements_visibility_check_auth()
-        with check:
-            page.fill_review('Отличный фильм на вечер')
-        with check:
-            page.check_pop_up_message()
+        page.open_via_auth(registered_user.email, registered_user.password)
+        page.all_elements_visibility_check_auth()
+        page.fill_review('Отличный фильм на вечер')
+        page.check_pop_up_message()
         time.sleep(5)
 
     @allure.title("Проверяем, что без авторизации, не можем оставить отзыв под фильмом")
     def test_feedback_page_not_auth(self, page: Page):
         page = CinescopReviewPage(page)
-        with check:
-            page.open()
-        with check:
-            page.all_elements_visibility_check_not_auth()
+        page.open()
+        page.all_elements_visibility_check_not_auth()
         time.sleep(5)
